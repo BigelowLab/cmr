204d91636cc7853df5afd81ad0a55c75ae01576e
Ben Tupper <btupper@bigelow.org>
2022-05-04 18:29:54 -0400

    get/create/delete token

--- R/credentials.R
+++ R/credentials.R
@@ -1,42 +1,91 @@
-#' Create a token
+#' Get a token
+#' 
+#' @seealso \href{https://wiki.earthdata.nasa.gov/display/CMR/CMR+Client+Partner+User+Guide#CMRClientPartnerUserGuide-Chapter2:GettingStarted}{CMR guide}
+#' @export
+#' @param x credentials list
+#' @param delete logical if TRUE delete exisiting token (if it exists) first
+#' @param ... other arguments for \code{\link{token_create}}
+#' @return updated credentials list
+get_token <- function(x = read_credentials(), delete = has_token(x), ...){
+  if (delete && has_token(x)) x <- token_delete(x)
+  x$token <- token_create(x, ...)
+  x
+}
+
+#' Determine is credentials has a non-empty token
 #' 
 #' @export
 #' @param x credentials list
-#' @param uri character, the URL
-#' @return token or NULL if fails
-token_create <- function(x = read_credentials(),
-                         uri = api_base("ops", segments = "api/users/token")){
-  # curl -X POST --header "Content-Type: application/xml" -d "<token><username>sample_username</username><password>sample-password</password><client_id>client_name_of_your_choosing</client_id><user_ip_address>your_origin_ip_address</user_ip_address> </token>" https://cmr.earthdata.nasa.gov/legacy-services/rest/tokens
-  #
-  # OR save mytokengenerator.xml
-  # 
-  # <token>
-  #   <username>sample_username</username>
-  #   <password>sample-password</password>
-  #   <client_id>client_name_of_your_choosing</client_id>
-  #   <user_ip_address>your_origin_ip_address</user_ip_address>
-  # </token>
-  #
-  # curl -X POST --header "Content-Type: application/xml" -d @mytokengenerator.xml https://cmr.uat.earthdata.nasa.gov/legacy-services/rest/tokens
-  
-  if (is.null(x[['client_id']])) x[['client_id']] <- x[['username']]
-  if (is.null(x[['user_ip_address']])) x[['user_ip_address']] <- pingr::my_ip()
-  if (!is.null(x[['token']])) x[['token']] <- NULL
+#' @return logical, TRUE is non-empty token exists in credentials
+has_token <- function(x = read_credentials()){
+  all(!is.null(x$token) && inherits(x$token, 'character') && (length(x$token) > 0))
+}
 
-  r <- httr::POST(uri, config = list(httr::add_headers(unlist(x))))
+#' Create a token
+#' 
+#' @seealso \href{https://wiki.earthdata.nasa.gov/display/CMR/CMR+Client+Partner+User+Guide#CMRClientPartnerUserGuide-Chapter2:GettingStarted}{CMR guide}
+#' @export
+#' @param x credentials list
+#' @param uri character, the URL
+#' @param verbose logical, if TRUE echo the command string
+#' @param use character one of 'curl' or 'httr'
+#' @return character token or NULL if fails
+token_create <- function(x = read_credentials(),
+                         verbose = FALSE,
+                         uri = api_base("ops", segments = "legacy-services/rest/tokens")){
+
+  y <- x[!(names(x) %in% "token")]
+  resp <- POST(uri,  
+               body = as_xml_string(y),
+               content_type_xml())
   
-  r
+  if (httr::http_error(resp)) {
+    print(resp)
+    warning(sprintf("error POSTing for token: %s"), uri)
+    retunr(NULL)
+  }
+  
+  r <- httr::content(resp)$token[['id']]  
+  return(r)
 }
 
 
 #' Delete a token
+#' 
+#' @seealso \href{https://wiki.earthdata.nasa.gov/display/CMR/CMR+Client+Partner+User+Guide#CMRClientPartnerUserGuide-Chapter2:GettingStarted}{CMR guide}
 #' @export
+#' @param x credentials list
+#' @param uri character, the URL
+#' @param verbose logical, if TRUE echo the command string
 #' @param x credentials list 
-token_delete <- function(x){
-  # nothing yet
+token_delete <- function(x = read_credentials(),
+                         uri = api_base("ops", segments = "legacy-services/rest/tokens"),
+                         verbose = FALSE){
+  
+  
+  
+  if (is.null(x$token)){
+    stop("token must be not be NULL")
+  }
+  if (!inherits(x$token, 'character')){
+    stop("token must be a character string")
+  }
+  if (length(x$token) == 0){
+    stop("token must be a non-zero length character string")
+  }
+
+  y <- x[!(names(x) %in% "token")]
+  resp <- httr::DELETE(file.path(uri,x$token), httr::content_type_xml())
+  
+  if (httr::http_error(resp)) {
+    print(resp)
+    warning(sprintf("error DELETEing token: %s"), uri)
+    retunr(NULL)
+  }
+  x[['token']] <- character()
+  
+  x
 }
-
-
 
 #' Read a credentials for EarthData
 #' 
@@ -47,10 +96,57 @@ token_delete <- function(x){
 #' 
 #' @export
 #' @param filename char, the name of the credentials file
+#' @param client_id character, can be anyhting, defaults to user name
+#' @param user_ip_address character, defaults to that provided by 
+#'   \code{\link[pingr]{my_ip}}
 #' @return named list of credentials
-read_credentials <- function(filename = "~/.earthdata"){
-  yaml::read_yaml(filename[1])
+read_credentials <- function(filename = "~/.earthdata",
+                             client_id = system2("whoami", stdout = TRUE),
+                             user_ip_address = pingr::my_ip(),
+                             form = c('list', "xml", "xml-string")[1]){
+  x <- c(yaml::read_yaml(filename[1]), 
+         client_id = client_id, 
+         user_ip_address = user_ip_address)
+  switch(tolower(form[1]),
+         "xml" = as_xml(x),
+         "xml-string" = as_xml_string(x),
+         x)
 }
+
+save_credentials <- function(x, filename = "~/.earthdata"){
+  must_have <- c("username", "password", "token")
+  y <- x[names(x) %in% must_have]
+  if (!all(names(y) %in% must_have)){
+    stop("credentials must have", paste(must_have, collapse = ", "))
+  }
+  yaml::write_yaml(y, filename)
+  x
+}
+
+#' Given a credentials list, convert to an xml node
+#' 
+#' @export
+#' @param x list of credentials from \code{\link{read_credentials}}
+#' @return xml node
+as_xml <- function(x = read_credentials()){
+  node <- xml2::xml_new_root("token") |> xml2::xml_root()
+  for (nm in names(x))  xml2::xml_add_child(node, nm, x[[nm]])
+  node
+}
+
+#' Given a credentials list, convert to an xml string
+#' 
+#' @export
+#' @param x list of credentials from \code{\link{read_credentials}}
+#' @return character, single element of XML
+as_xml_string <- function(x = read_credentials()){
+  node <- as_xml(x)
+  tmp <- tempfile(fileext = ".xml")
+  xml2::write_xml(node, tmp, options = "format")
+  readLines(tmp)[-1] |> trimws(which = "both") |> paste(collapse = "")
+}
+
+
 
 #' Write a credentials for EarthData
 #' 
