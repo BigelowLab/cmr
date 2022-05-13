3988e71a7211379aeffa2f7f9d43957d23a9fd6f
Ben Tupper <btupper@bigelow.org>
2022-05-13 11:09:03 -0400

    update fetch.R

--- R/fetch.R
+++ R/fetch.R
@@ -1,8 +1,13 @@
 #https://cmr.earthdata.nasa.gov/search/granules.umm_json?collection_concept_id=C1625128926-GHRC_CLOUD&temporal=2019-01-01T10:00:00Z,2019-12-31T23:59:59Z
 
-fetch <- function(
-    collection_concept_id = "C1996881146-POCLOUD",
-    temporal = NULL,
-    
-    
-  cred = get_token())
+fetch <- function(uri = search_collection() |> 
+                    dplyr::filter(grepl("OPENDAP", .data$Subtype, fixed = TRUE)) |>
+                    dplyr::slice(1) |>
+                    dplyr::pull(.data$URL),
+  times = c("2019-01-01T10:00:00Z","2019-02-01T00:00:00Z"),
+  bbox = c(-180, -90, 180, 90),
+  cred = get_token()){
+  
+  
+  
+}
